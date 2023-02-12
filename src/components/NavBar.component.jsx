c15f5a837e0fe0a2e9946a9bf1492b1e5b6e4a18
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-12 21:19:57 +0100

    did some changes on navbar styling

--- src/components/NavBar.component.jsx
+++ src/components/NavBar.component.jsx
@@ -9,9 +9,12 @@ export default function NavBar() {
       <div className="nav-context flex gap-4 items-center">
         <UserMenu />
         <div>
-          <div className="cart h-10 px-4 rounded-full flex items-center cursor-pointer border-2 border-red-700 ">
+          <div className="cart h-10 pl-4 rounded-full flex items-center cursor-pointer border-2 border-red-700 ">
             <ShoppingCartIcon className="w-6 h-6  text-red-700" />
-            <div className="px-3 pointer-events-none">3.50</div>
+            <div className="px-3 pointer-events-none">
+              3.50
+              <span className="pl-1">€</span>
+            </div>
           </div>
         </div>
       </div>
