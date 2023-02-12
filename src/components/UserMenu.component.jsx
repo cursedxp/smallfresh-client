dc467b824ab82ff95208e27572095767acbfb8c7
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-12 20:52:05 +0100

    selection event disabled on Usermenu

--- src/components/UserMenu.component.jsx
+++ src/components/UserMenu.component.jsx
@@ -29,26 +29,26 @@ export default function UserMenu() {
   }, [showMenu]);
 
   return (
-    <div className="menu relative">
+    <div className="menu relative ">
       <div
         onClick={handleClick}
         className="user h-10 bg-slate-200 rounded-full flex items-center cursor-pointer"
       >
         <UserCircleIcon className="w-10 h-10 text-red-700" />
-        <div className="px-3 font-bold">John Doe</div>
+        <div className="px-3 font-bold pointer-events-none">John Doe</div>
       </div>
       {showMenu && (
-        <div className="context-menu z-10 absolute top-10 w-48 right-0 p-4 text-left rounded-2xl bg-white shadow-md">
+        <div className="context-menu z-10 absolute top-10 w-44 right-0 p-4 text-left rounded-2xl bg-white shadow-md">
           <ul className="text-slate-400">
-            <li className="flex gap-2 rounded-lg p-2 hover:bg-slate-100 hover:text-red-700 hover:font-bold">
+            <li className="flex gap-4 rounded-lg p-2 hover:bg-slate-100 hover:text-red-700 hover:font-bold">
               <QueueListIcon className="w-6 h-6 " />
               Orders
             </li>
-            <li className="mt-4 flex gap-2 rounded-lg p-2 hover:bg-slate-100 hover:text-red-700 hover:font-bold">
+            <li className="mt-4 flex gap-4 rounded-lg p-2 hover:bg-slate-100 hover:text-red-700 hover:font-bold">
               <MapPinIcon className="w-6 h-6" />
               Addresses
             </li>
-            <li className="mt-4 flex gap-2 rounded-lg p-2 hover:bg-slate-100 hover:text-red-700 hover:font-bold">
+            <li className="mt-4 flex gap-4 rounded-lg p-2 hover:bg-slate-100 hover:text-red-700 hover:font-bold">
               <Cog6ToothIcon className="w-6 h-6 " />
               Settings
             </li>
