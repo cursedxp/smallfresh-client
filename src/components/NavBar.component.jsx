a6deae7fa6f32ab213b87761eaab52cdcb85efaa
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-12 21:02:13 +0100

    cart element has been added to nav bar

--- src/components/NavBar.component.jsx
+++ src/components/NavBar.component.jsx
@@ -1,12 +1,19 @@
 import NavBranding from "./Branding.component";
 import UserMenu from "./UserMenu.component";
+import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 
 export default function NavBar() {
   return (
     <nav className="navigation w-full p-4 flex justify-between">
       <NavBranding />
-      <div className="nav-context flex items-center">
+      <div className="nav-context flex gap-4 items-center">
         <UserMenu />
+        <div>
+          <div className="cart h-10 px-4 rounded-full flex items-center cursor-pointer border-2 border-red-700 ">
+            <ShoppingCartIcon className="w-6 h-6  text-red-700" />
+            <div className="px-3 pointer-events-none">3.50</div>
+          </div>
+        </div>
       </div>
     </nav>
   );
