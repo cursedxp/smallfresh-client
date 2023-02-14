de101c299db9e880363077f4bd05aa4bfc33107f
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-14 11:12:22 +0100

    home page has been added to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar.component";
+import HomePage from "./pages/Home.pages";
 
 export default function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes path="/">
-        <Route></Route>
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
       </Routes>
     </div>
   );
---
+++ src/pages/Home.pages.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <div
+      className="m-4 rounded-3xl"
+      style={{
+        height: 400,
+        backgroundImage: `url(${"https://images.unsplash.com/photo-1506365069540-904bcc762636"})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    ></div>
+  );
+}
