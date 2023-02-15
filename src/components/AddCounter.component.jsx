dc0c899f5b2b3a0d10d80447267d9c74e76186b1
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-15 20:00:30 +0100

    AddCounter component has been created

--- src/components/AddCounter.component.jsx
+++ src/components/AddCounter.component.jsx
@@ -1,29 +1,25 @@
 import { PlusIcon, MinusIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
 
-export default function AddCounter() {
-  const [piece, setPiece] = useState(0);
+export default function AddCounter(props) {
+  const { count, onAdd, onRemove } = props;
 
   return (
-    <div className="add-many-button flex ">
+    <div className="add-many-button flex">
       <button
-        className="bg-red-700 p-3 rounded-l-xl text-white"
-        onClick={() => {
-          setPiece((prevPiece) => {
-            return prevPiece + -1;
-          });
-        }}
+        className="bg-red-700 p-2 rounded-l-xl text-white"
+        onClick={onRemove}
       >
         <MinusIcon className="w-6 h-6" />
       </button>
-      <input type="text" value={piece} className="w-12 text-right text-lg" />
+      <input
+        type="text"
+        value={count}
+        className="w-12 text-right text-lg"
+        readOnly
+      />
       <button
-        className="bg-red-700 p-3 rounded-r-xl text-white"
-        onClick={() => {
-          setPiece((prevPiece) => {
-            return prevPiece + 1;
-          });
-        }}
+        className="bg-red-700 p-2 rounded-r-xl text-white"
+        onClick={onAdd}
       >
         <PlusIcon className="w-6 h-6" />
       </button>
