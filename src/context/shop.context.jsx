4e80ed2cffb0b1f86721c35ec6fc23a54298649e
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-18 10:08:56 +0100

    basket state added to context

--- src/context/shop.context.jsx
+++ src/context/shop.context.jsx
@@ -1,9 +1,14 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const ShopContext = createContext();
 
 function ShopContextWrapper(props) {
-  return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
+  const [basket, setBasket] = useState;
+  return (
+    <ShopContext.Provider setBasket={setBasket}>
+      {props.children}
+    </ShopContext.Provider>
+  );
 }
 
 export { ShopContext, ShopContextWrapper };
