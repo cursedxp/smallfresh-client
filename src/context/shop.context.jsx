b4518fb1ce23ca5539414c714ed39aa95fbdbbef
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-18 10:36:25 +0100

    states has been added to context for auth

--- src/context/shop.context.jsx
+++ src/context/shop.context.jsx
@@ -4,6 +4,10 @@ const ShopContext = createContext();
 
 function ShopContextWrapper(props) {
   const [basket, setBasket] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
   return (
     <ShopContext.Provider setBasket={setBasket}>
       {props.children}
