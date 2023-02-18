d95556be8618a32e83727644218f4146b4f516bd
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-18 14:51:48 +0100

    auth context update

--- src/context/shop.context.jsx
+++ src/context/shop.context.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const ShopContext = createContext();
 
 function ShopContextWrapper(props) {
@@ -8,8 +9,64 @@ function ShopContextWrapper(props) {
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
 
+  const navigate = useNavigate();
+  const storeToken = (token) => {
+    localStorage.setItem("authToken", token);
+  };
+
+  const authenticateUser = () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    if (storedToken) {
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/auth/verify`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+
+        .then((response) => {
+          const user = response.data;
+          setIsLoggedIn(true);
+          setIsLoading(false);
+          setUser(user);
+        })
+        .catch((error) => {
+          setIsLoggedIn(false);
+          setIsLoading(false);
+
+          setUser(null);
+        });
+    } else {
+      setIsLoggedIn(false);
+      setIsLoading(false);
+      setUser(null);
+    }
+  };
+  const removeToken = () => {
+    localStorage.removeItem("authToken");
+  };
+
+  const logOutUser = () => {
+    removeToken();
+    authenticateUser();
+    navigate("/");
+  };
+
+  useEffect(() => {
+    authenticateUser();
+  }, []);
+
   return (
-    <ShopContext.Provider value={{ setBasket, isLoading, isLoggedIn, user }}>
+    <ShopContext.Provider
+      value={{
+        setBasket,
+        isLoading,
+        isLoggedIn,
+        user,
+        storeToken,
+        authenticateUser,
+        logOutUser,
+      }}
+    >
       {props.children}
     </ShopContext.Provider>
   );
