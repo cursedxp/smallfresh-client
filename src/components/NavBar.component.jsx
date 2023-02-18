0d8ebaae00742d6a62dd85c35a40acba06ad6815
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-18 10:55:58 +0100

    auth states added to navbar

--- src/components/NavBar.component.jsx
+++ src/components/NavBar.component.jsx
@@ -1,20 +1,38 @@
 import NavBranding from "./Branding.component";
 import UserMenu from "./UserMenu.component";
 import Cart from "./Cart.component";
-import { HeartIcon } from "@heroicons/react/24/solid";
+import { HeartIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import { useContext } from "react";
 import { ShopContext } from "../context/shop.context";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
+  const { isLoggedIn, user } = useContext(ShopContext);
+
   return (
     <nav className="navigation w-full p-4 flex justify-between">
       <NavBranding />
       <div className="nav-context flex gap-6 items-center">
-        <UserMenu />
-        <div className="flex gap-2 items-center hover:shadow-xl h-10 hover:text-red-700 rounded-full px-4 cursor-pointer">
-          <HeartIcon className="w-6 h-6 text-red-700 stroke-2" />
-          <span>My products</span>
-        </div>
+        {isLoggedIn && (
+          <>
+            <UserMenu />
+            <div className="flex gap-2 items-center hover:shadow-xl h-10 hover:text-red-700 rounded-full px-4 cursor-pointer">
+              <HeartIcon className="w-6 h-6 text-red-700 stroke-2" />
+              <span>My products</span>
+            </div>
+          </>
+        )}
+        {!isLoggedIn && (
+          <>
+            <div className="user h-10 hover:text-red-700 rounded-full gap-2 flex items-center">
+              <div className="mr-4 pointer-events-none text-red-700">
+                <Link to="/signup">Sign Up</Link>
+                <span className="mx-2">/</span>
+                <Link to="/login">Login</Link>
+              </div>
+            </div>
+          </>
+        )}
         <Cart />
       </div>
     </nav>
