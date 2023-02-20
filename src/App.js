9115cf127b99648ba1f173ca31c6109acfc645ab
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-20 15:45:16 +0100

    Product details page added to routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar.component";
 import HomePage from "./pages/Home.pages";
 import SignUp from "./pages/SignUp.pages";
 import Login from "./pages/Login.pages";
+import ProductDetails from "./pages/ProductDetails.pages";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/products/:productId" element={<ProductDetails />} />
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
