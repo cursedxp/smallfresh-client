f8458af35fd129df5214b5f0cf2edaa6c141c9ce
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-20 15:46:34 +0100

    Link added to your products component

--- src/components/YourProducts.compopnents.jsx
+++ src/components/YourProducts.compopnents.jsx
@@ -1,7 +1,9 @@
 import ProductCard from "./ProductCard.component";
+import { Link } from "react-router-dom";
 
 export default function YourProducts(props) {
   const { products } = props;
+
   return (
     <div className="w-full bg-center">
       <h3 className="font-bold text-white text-2xl text-left mb-4">
@@ -9,7 +11,14 @@ export default function YourProducts(props) {
       </h3>
       <div className="grid grid-rows-1 grid-flow-col gap-4">
         {products.map((product) => {
-          return <ProductCard key={product._id} product={product} />;
+          return (
+            <Link
+              key={product._id}
+              to={{ pathname: `/products/${product._id}`, state: { product } }}
+            >
+              <ProductCard product={product} />
+            </Link>
+          );
         })}
       </div>
     </div>
