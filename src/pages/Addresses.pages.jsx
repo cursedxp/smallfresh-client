d0f8e81ddf0e8b7903459aa269232b6c6ba10496
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-21 12:16:27 +0100

    addresses page has been created

---
+++ src/pages/Addresses.pages.jsx
@@ -0,0 +1,13 @@
+export default function Addresses() {
+  return (
+    <div
+      className="container m-4 rounded-3xl  items-center flex flex-col p-8 pt-12 "
+      style={{
+        height: 200,
+        backgroundImage: `url("https://images.unsplash.com/photo-1609196141706-91f4f03e1543")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    ></div>
+  );
+}
