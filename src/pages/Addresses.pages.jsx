ed9b464561b11cd689ea76775a10ac6b4533848a
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-21 12:57:48 +0100

    address page has been created

--- src/pages/Addresses.pages.jsx
+++ src/pages/Addresses.pages.jsx
@@ -1,13 +1,17 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function Addresses() {
+  const [addresses, setAddresses] = useState();
+
   return (
-    <div
-      className="container m-4 rounded-3xl  items-center flex flex-col p-8 pt-12 "
-      style={{
-        height: 200,
-        backgroundImage: `url("https://images.unsplash.com/photo-1609196141706-91f4f03e1543")`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    ></div>
+    <div className="flex flex-col w-full">
+      <div className="flex justify-between border-b">
+        <h3 className="font-bold text-2xl text-left mt-4 mb-4 ">Addresses</h3>
+        <button className="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-6 rounded-xl h-12 focus:outline-none focus:shadow-outline">
+          Add New
+        </button>
+      </div>
+    </div>
   );
 }
