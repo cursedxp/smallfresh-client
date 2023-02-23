bb5d68776155e698cd2d3e940285329ce2e28580
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-23 18:45:37 +0100

    test string added to my products page

--- src/pages/MyProducts.pages.jsx
+++ src/pages/MyProducts.pages.jsx
@@ -1,3 +1,7 @@
 export default function MyProducts() {
-  return <div className="my-products"></div>;
+  return (
+    <div className="my-products">
+      <h3>Hello My products</h3>
+    </div>
+  );
 }
