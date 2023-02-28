0440612547321a3b19c2391d4e002833d0433cda
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-28 09:50:23 +0100

    add product functioality moved to cards component

--- src/components/ProductCard.component.jsx
+++ src/components/ProductCard.component.jsx
@@ -1,14 +1,40 @@
 import LikeButton from "./LikeButton.component";
-import AddMoreProducts from "./AddMoreProducts.component";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import AddCounter from "./AddCounter.component";
+import AddItemButton from "./AddItemButton.component";
+import { useContext } from "react";
+import { ShopContext } from "../context/shop.context";
 
 export default function ProductCard(props) {
+  const { basket, setBasket } = useContext(ShopContext);
+
   const { product } = props;
+  const [count, setCount] = useState(0);
+
+  const handleAddItem = () => {
+    setCount(count + 1);
+  };
+  const handleRemoveItem = () => {
+    if (count > 0) {
+      setCount(count - 1);
+      setBasket(count);
+    }
+  };
+
+  useEffect(() => {
+    setBasket(count);
+  }, [count]);
+
   return (
     <div className="product-card flex flex-col bg-white p-8 rounded-3xl shadow-lg">
       <Link
-        to={{ pathname: `/products/${product._id}`, state: { product } }}
-        className="flex flex-col"
+        to={{
+          pathname: `/products/${product._id}`,
+          state: {
+            count: count,
+          },
+        }}
       >
         <img src={product.img} alt={product.name} />
         <div className=" self-start text-xl">{product.name}</div>
@@ -22,7 +48,17 @@ export default function ProductCard(props) {
       </Link>
       <div className="flex justify-between">
         <LikeButton product={product} />
-        <AddMoreProducts />
+        <div>
+          {count > 0 ? (
+            <AddCounter
+              count={count}
+              onAdd={handleAddItem}
+              onRemove={handleRemoveItem}
+            />
+          ) : (
+            <AddItemButton onClick={handleAddItem} />
+          )}
+        </div>
       </div>
     </div>
   );
